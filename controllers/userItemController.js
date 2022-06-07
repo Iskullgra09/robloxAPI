6b29517a9f370ff3775e185d47372df23fa30443
Isaac Granados <isaac_granados09@hotmail.com>
2022-06-07 10:54:14 -0600

    -Change patch to put

--- controllers/userItemController.js
+++ controllers/userItemController.js
@@ -38,7 +38,7 @@ router.post('/createUserItem', async(req, res,next) => {
     }
 });
 
-router.patch('/modifyUserItem', async(req, res,next) => {
+router.put('/modifyUserItem', async(req, res,next) => {
     let connection = mysql.createConnection(mysqlconfig);
     const db = makeDb();
     await db.connect(connection);
