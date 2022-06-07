4ecd89e807583fd3f567706d9ae56b85b698b71a
Isaac Granados <isaac_granados09@hotmail.com>
2022-06-06 23:16:11 -0600

    -Update del config

--- connectionMySQL/config.js
+++ connectionMySQL/config.js
@@ -1,8 +1,17 @@
-var config = {
+/*var config = {
     user: 'root',
     password: 'Sa1234567890',
     host: 'localhost',
     database: 'robloxdb',
     connectionLimit: 10,
+}*/
+
+var config = {
+    user: 'mydbs_robloxuser',
+    password: 'robloxUser',
+    host: 'mysql-mydbs.alwaysdata.net',
+    database: 'mydbs_roblox',
+    connectionLimit: 10,
 }
+
 module.exports = config;
