3c193eaf06ee52f88e3701a46604794a7a6ac9f3
Isaac Granados <isaac_granados09@hotmail.com>
2022-06-06 21:41:52 -0600

    -CRUD user items

--- controllers/userItemController.js
+++ controllers/userItemController.js
@@ -2,31 +2,70 @@ var express = require('express');
 var router = express.Router();
 var ErrorHanlder = require('../models/error-class');
 var HttpStatus = require('http-status-codes');
+var mysql = require('mysql');
+var mysqlconfig = require('../connectionMySQL/config');
+const { makeDb } = require('mysql-async-simple');
 
-const userItemManager = require('../managers/userItemManager');
+router.get('/getUserItems', async(req, res,next) => {
 
-router.get('/getUserItems', (req, res,next) => {
-    res.status(HttpStatus.OK).json({ data: req.query});
-    userItemManager.getUserItems(req.query)
-      .then(result => {
-          //res.status(HttpStatus.OK).json({ data: req.query});
-        /*if (result.output.success) res.status(HttpStatus.OK).json({ data: result.recordset});
-        else return next(new ErrorHanlder('', HttpStatus.BAD_REQUEST));*/
-      })
-      .catch(err => {
-        return next(new ErrorHanlder(err.message, HttpStatus.INTERNAL_SERVER_ERROR));
-      })
+    let connection = mysql.createConnection(mysqlconfig);
+    const db = makeDb();
+    await db.connect(connection);
+
+    try {
+        const usersItems = await db.query(connection, 'CALL getUserItems(?)', [req.query.userID]);
+        console.log(usersItems)
+        res.status(HttpStatus.OK).json({ data: usersItems[0], success: true});
+    } catch (e) {
+        res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ data: 'Error al obtener', success: false});
+    } finally {
+        await db.close(connection);
+    }
 });
 
-router.post('/createUserItem', (req, res,next) => {
-    exampleManager.exampleFunct(req.query)
-        .then(result => {
-            if (result.output.success) res.status(HttpStatus.OK).json({ data: result.recordset});
-            else return next(new ErrorHanlder('', HttpStatus.BAD_REQUEST));
-        })
-        .catch(err => {
-            return next(new ErrorHanlder(err.message, HttpStatus.INTERNAL_SERVER_ERROR));
-        })
+router.post('/createUserItem', async(req, res,next) => {
+    let connection = mysql.createConnection(mysqlconfig);
+    const db = makeDb();
+    await db.connect(connection);
+
+    try {
+        const usersItems = await db.query(connection, 'CALL createUserItem(?,?,?)', [req.body.userID, req.body.itemID, req.body.itemXP]);
+        res.status(HttpStatus.OK).json({success: true});
+    } catch (e) {
+        res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ data: 'Error al crear', success: false});
+    } finally {
+        await db.close(connection);
+    }
+});
+
+router.patch('/modifyUserItem', async(req, res,next) => {
+    let connection = mysql.createConnection(mysqlconfig);
+    const db = makeDb();
+    await db.connect(connection);
+
+    try {
+        const usersItems = await db.query(connection, 'CALL modifyUserItem(?,?)', [req.body.ID, req.body.itemXP]);
+        res.status(HttpStatus.OK).json({success: true});
+    } catch (e) {
+        res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ data: 'Error al modificar', success: false});
+    } finally {
+        await db.close(connection);
+    }
+});
+
+router.delete('/deleteUserItem', async(req, res,next) => {
+    let connection = mysql.createConnection(mysqlconfig);
+    const db = makeDb();
+    await db.connect(connection);
+
+    try {
+        const usersItems = await db.query(connection, 'CALL deleteUserItem(?)', [req.body.ID]);
+        res.status(HttpStatus.OK).json({success: true});
+    } catch (e) {
+        res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ data: 'Error al borrar', success: false});
+    } finally {
+        await db.close(connection);
+    }
 });
 
 module.exports = router;
